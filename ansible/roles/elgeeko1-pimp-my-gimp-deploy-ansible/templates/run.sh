3f6896b7fddea71cfa5b733366c1d593597abad4
elgeeko1 <jeff@nullptr.com>
2023-10-19 19:03:47 -0600

    Update pimp-my-gimp service to show unbuffered output from Python.

--- ansible/roles/elgeeko1-pimp-my-gimp-deploy-ansible/templates/run.sh
+++ ansible/roles/elgeeko1-pimp-my-gimp-deploy-ansible/templates/run.sh
@@ -3,4 +3,4 @@
 echo PWD=$PWD 
 
 source {{ gimp_path }}/venv/bin/activate
-sudo -E env PATH=$PATH python {{ gimp_path }}/pimp-my-gimp.py
+sudo -E env PATH=$PATH python -u {{ gimp_path }}/pimp-my-gimp.py
