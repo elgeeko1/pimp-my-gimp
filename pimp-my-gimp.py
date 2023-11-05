b142cea8d94ccfebfd300eba6a63ab66f2bb0fae
elgeeko1 <jeff@nullptr.com>
2023-11-05 13:37:55 -0700

    Replace complex and computationally intensive interpolaation smoothing algorithm with simple exponential moving average, and implement circular buffers in numpy

--- pimp-my-gimp.py
+++ pimp-my-gimp.py
@@ -30,12 +30,165 @@ from flask import send_from_directory  # send raw file
 from flask import jsonify  # graphing
 
 # maths for graphing
-import numpy as np
-from scipy.interpolate import interp1d
+import numpy
 
 # telegraf
 import requests
 import urllib3
+
+import numpy
+
+class TimeStampedCircularBuffer:
+    """
+    A circular buffer that holds timestamped data entries.
+    Once the buffer reaches its capacity, it starts overwriting the oldest data.
+    """
+    def __init__(self, capacity: int):
+        """
+        Initializes the buffer with a given capacity.
+        
+        :param capacity: The maximum number of timestamped data entries the buffer can hold.
+        """
+        self._capacity = capacity
+        self._buffer = numpy.empty((0, 2), float)  # Initialize an empty 2D array for timestamp and data
+        self._head = 0  # Points to the start of the buffer (write head)
+
+    def append(self, timestamp: float, value: float) -> None:
+        """
+        Appends a new timestamped data entry to the buffer.
+
+        :param timestamp: The timestamp of the data point.
+        :param value: The data value to store.
+        """
+        if self._buffer.shape[0] < self._capacity:
+            self._buffer = numpy.vstack((self._buffer, numpy.array([timestamp, value])))
+        else:
+            self._buffer[self._head] = [timestamp, value]
+            self._head = (self._head + 1) % self._capacity
+
+    def last(self) -> numpy.ndarray:
+        """
+        Returns the last appended data entry.
+
+        :return: The last data entry in the buffer.
+        """
+        return self._buffer[(self._head - 1) % self._capacity]
+
+    def values(self) -> numpy.ndarray:
+        """
+        Retrieves all the timestamped data entries in the buffer, ordered by the time they were added.
+        This method takes into account the circular nature of the buffer to return the values in the correct order.
+
+        :return: A numpy array of timestamped data entries, where each entry is a [timestamp, value] pair.
+        """
+        return numpy.roll(self._buffer, -self._head, axis=0)
+
+
+class ExponentialSmoothing:
+    """
+    Exponential smoothing algorithm for time series data.
+    """
+    def __init__(self, alpha: float = 0.5):
+        """
+        Initializes the exponential smoothing filter.
+
+        :param alpha: The smoothing factor, a value between 0 and 1.
+        """
+        self._alpha = alpha
+        self._last_smoothed = None
+
+    def smooth(self, value: float) -> float:
+        """
+        Applies exponential smoothing to the given value.
+
+        :param value: The data value to be smoothed.
+        :return: The smoothed data value.
+        """
+        if self._last_smoothed is None:
+            self._last_smoothed = value
+        else:
+            self._last_smoothed = self._alpha * value + (1 - self._alpha) * self._last_smoothed
+
+        return self._last_smoothed
+    
+    def value(self) -> float:
+        """
+        Returns the last smoothed value.
+
+        :return: The last smoothed data value.
+        """
+        return self._last_smoothed
+
+class Trajectory:
+    """
+    The Trajectory class calculates and stores the position and speed of an object over time.
+    It uses a TimeStampedCircularBuffer to maintain a fixed-size window of the latest position and speed data points
+    and applies exponential smoothing to the speed data to reduce noise and variability in the measurements.
+    """
+
+    def __init__(self, window_size: int, alpha: float = 0.5):
+        """
+        Initializes the Trajectory with a specified window size for the buffers and a smoothing factor for speed calculation.
+
+        :param window_size: The maximum number of entries for the position and speed graphs.
+        :param alpha: The smoothing factor used for exponential smoothing of the speed data.
+        """
+        self._last_timestamp = time.time()  # Stores the timestamp of the last update
+        self._last_position = 0.0  # Stores the last calculated position
+        self._last_speed = 0.0  # Stores the last calculated speed
+        self._position_graph = TimeStampedCircularBuffer(window_size)  # Circular buffer for position data
+        self._speed_graph = TimeStampedCircularBuffer(window_size)  # Circular buffer for speed data
+        self._speed_filter = ExponentialSmoothing(alpha)  # Exponential smoothing filter for speed
+
+    def step(self, step_pulses: float = 1.0, offset_s: float = 0.0):
+        """
+        Updates the position and speed based on the step pulses received since the last update.
+
+        :param step_pulses: The number of pulses since the last update, which is proportional to the distance moved.
+        :param offset_s: The time offset in seconds to be added to the current time, for timestamping the update.
+        """
+        timestamp = time.time() + offset_s
+        dt = timestamp - self._last_timestamp
+        new_position = self._last_position + step_pulses
+        new_speed = self._speed_filter.smooth(step_pulses / dt)
+        self._position_graph.append(timestamp, new_position)
+        self._speed_graph.append(timestamp, new_speed)
+        self._last_timestamp = timestamp
+        self._last_position = new_position
+        self._last_speed = new_speed
+
+    def position(self) -> (float, float):
+        """
+        Returns the last recorded position and its associated timestamp.
+
+        :return: A tuple containing the last timestamp and the last position.
+        """
+        return self._last_timestamp, self._last_position
+
+    def positions(self) -> numpy.ndarray:
+        """
+        Retrieves all the recorded positions from the position graph.
+
+        :return: A numpy array of timestamped position entries.
+        """
+        return self._position_graph.values()
+
+    def speed(self) -> (float, float):
+        """
+        Returns the last recorded speed and its associated timestamp.
+
+        :return: A tuple containing the last timestamp and the last speed.
+        """
+        return self._last_timestamp, self._last_speed
+
+    def speeds(self) -> numpy.ndarray:
+        """
+        Retrieves all the recorded speeds from the speed graph.
+
+        :return: A numpy array of timestamped speed entries.
+        """
+        return self._speed_graph.values()
+    
 
 # NeoPixel communication over GPIO 18 (pin 12)
 PIXEL_PIN = board.D18
@@ -53,10 +206,11 @@ ENCODER_PULSES_PER_REV = 4
 # Encoder pulses per linear foot
 # wheel diameter is 7.5", so circumference is pi * 7.5
 ENCODER_PULSES_PER_FOOT = float(ENCODER_PULSES_PER_REV) / (math.pi * (7.5/12.0))
-# Encoder count. Updated asynchronously.
-ENCODER_COUNT = 0.0
-# Graph of encoder speed (timestamp, position). Updated asynchronously
-ENCODER_GRAPH = []
+# Encoder speed smoothing coefficient (for exponential moving average)
+ENCODER_SMOOTHING = 0.75
+
+# position traveled, in encoder pulses
+trajectory = Trajectory(ENCODER_WINDOW_PULSES, ENCODER_SMOOTHING)
 
 # initialize the pixels array
 #   return: pixel array
@@ -135,70 +289,20 @@ def pixels_solid(pixels: neopixel.NeoPixel, color: tuple = COLOR_IDLE):
     pixels.show()
 
 
-def smooth_timeseries(timestamps: np.array, 
-                      values: np.array, 
-                      window_size: int, 
-                      interp_spacing_s: float) -> list[(float, float)]:
-    """
-    Smooths a given time series data using linear interpolation followed by a moving average filter.
-    
-    Parameters:
-    - timestamps: A numpy array of timestamps (in seconds since epoch time).
-    - values: A numpy array of corresponding values.
-    - window_size: The size of the moving average window, in samples. This determines the smoothing degree.
-    - interp_spacing_s: The spacing in seconds for interpolation. Determines the interval of the resulting timestamps.
-    
-    Returns:
-    - A list of tuples containing smoothed timestamps and their corresponding smoothed data values.
-    
-    Note:
-    - The returned timestamps are interpolated based on the provided interp_spacing_s, and are not the same as the original timestamps.
-    """
-    
-    # Generate new equispaced timestamps based on interp_spacing_s
-    interpolated_timestamps = np.arange(timestamps[0], timestamps[-1], interp_spacing_s)
-
-    # Create a linear interpolation function for the given timestamps and values
-    f = interp1d(timestamps, values, kind='linear', fill_value='extrapolate', bounds_error=False)
-    
-    # Calculate interpolated values for the newly generated equispaced timestamps
-    interpolated_values = f(interpolated_timestamps)
-
-    # Check if interpolated values are available, if not, return an empty list
-    if len(interpolated_values) == 0:
-        return np.array([]), np.array([])
-    
-    # Apply moving average filter on the interpolated values for smoothing
-    smoothed_values = np.convolve(interpolated_values, np.ones(window_size)/window_size, mode='valid')
-    
-    # Adjust the start and end of the timestamps to match the length of smoothed_values due to convolution
-    smoothed_timestamps = interpolated_timestamps[:len(smoothed_values)]
-    
-    # Return the valid timestamps with their corresponding smoothed values as a list of tuples
-    return smoothed_timestamps, smoothed_values
-
-
 # encoder init
 def encoder_init():
-    global ENCODER_COUNT
-    global ENCODER_GRAPH
+    trajectory = Trajectory(ENCODER_WINDOW_PULSES, ENCODER_SMOOTHING)
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(ENCODER_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(ENCODER_PIN, GPIO.RISING, callback=encoder_handler)
-    ENCODER_COUNT = 0.0
-    ENCODER_GRAPH.append((time.time(), ENCODER_COUNT))
-
 
 # encoder edge event callback
 # called automatically in a separate thread on encoder pulses
 #   channel: event channel
 def encoder_handler(channel: int):
-    global ENCODER_COUNT
-    global ENCODER_GRAPH
-    ENCODER_COUNT += 1.0
-    timestamp = time.time()
-    ENCODER_GRAPH.append((timestamp, ENCODER_COUNT))
-    send_data_to_telegraf("distance_ft", (timestamp, 1.0 / ENCODER_PULSES_PER_FOOT))
+    global trajectory
+    trajectory.step(1.0)
+    # send_data_to_telegraf("distance_ft", (timestamp, 1.0 / ENCODER_PULSES_PER_FOOT))
 
 
 def send_data_to_telegraf(series: str, datapoint):
@@ -276,38 +380,24 @@ def run_web_server(pixels: neopixel.NeoPixel):
     # speed graph endpoint
     @app.route("/speed")
     def speed():
-        global ENCODER_GRAPH
-        # if last encoder pulse is more than two seconds old,
-        # assume zero velocity and append a datapoint to the graph one second back
-        if time.time() - ENCODER_GRAPH[-1][0] > 2.0:
-            ENCODER_GRAPH.append((time.time() - 1.0, ENCODER_COUNT))
-        # trim to graph window by dropping old timestamps
-        while len(ENCODER_GRAPH) > ENCODER_WINDOW_PULSES:
-            ENCODER_GRAPH.pop(0)
+        global trajectory
 
+        # if last encoder pulse was seen more than two seconds ago, assume zero speed
+        # append a zero step to the encoder graph one second ago
+        # the one-second latency for adding zero values prevents artifacts should
+        # an encoder pulse come in near the current time
+        stopped_threshold_s = 1.0
+        timestamp, speed = trajectory.speed()
+        if time.time() - timestamp > stopped_threshold_s:
+            trajectory.step(0.0, - stopped_threshold_s / 2)
+
+        speed_graph = trajectory.speeds()
         plotly_data =  {
             'type': 'scatter',
-            'x': [],
-            'y': [],
+            'x': speed_graph[:, 0].tolist() if len(speed_graph) else [],
+            'y': speed_graph[:, 1].tolist() if len(speed_graph) else [],
             'mode': 'lines'
         }
-        # copy volatile array
-        position = np.array(ENCODER_GRAPH)
-        # calculate and smooth the derivative of the encoder position graph
-        if len(position) >= 2:
-            # Calculate the differences in x and y values
-            differences = np.diff(position, axis=0)
-
-            # Calculate the derivative using the difference method
-            derivatives = differences[:, 1] / differences[:, 0]
-
-            (x,y) = smooth_timeseries(position[1:,0], derivatives, 2, 0.33)
-            plotly_data =  {
-                'type': 'scatter',
-                'x': x.tolist(),
-                'y': y.tolist(),
-                'mode': 'lines'
-            }
 
         return jsonify(data = plotly_data)
     
